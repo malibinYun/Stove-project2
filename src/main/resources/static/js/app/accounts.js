f5bbea4647575dce1e23e908c3b07041d9568867
Malibin <nightmare73@naver.com>
2020-12-18 18:15:07 +0900

    [Feat] 권한 변경 기능 완성

--- src/main/resources/static/js/app/accounts.js
+++ src/main/resources/static/js/app/accounts.js
@@ -1,6 +1,7 @@
 const accounts = {
     init: function () {
         this.loadAccounts()
+
         $('#button-logout').on('click', () => {
             this.logout()
         })
@@ -16,30 +17,17 @@ const accounts = {
             },
             contentType: 'application/json; charset=utf-8',
         }).done(function (response) {
-
-            const data = {
-                accounts: response
-            }
-            const template =
-                '{{#accounts}}\n' +
-                '    <tr>\n' +
-                '        <td>{{id}}</td>\n' +
-                '        <td>{{accountId}}</td>\n' +
-                '        <td>{{nickName}}</td>\n' +
-                '        <td>{{isAdmin}}</td>\n' +
-                '        <td>{{joinDate}}</td>\n' +
-                '    </tr>\n' +
-                '{{/accounts}}'
-
+            const data = {accounts: response}
+            const template = getAccountsTemplate()
             const output = Mustache.render(template, data)
             $('#tbody').html(output)
+            initPermissionButtons()
 
         }).fail(function (error) {
             const response = JSON.parse(error.responseText)
-            if(response.message === '권한이 없습니다.'){
-                console.log("??")
+            if (response.message === '권한이 없습니다.') {
                 window.location.href = "/mypage"
-            }else{
+            } else {
                 alert(response.message);
                 window.location.href = "/"
             }
@@ -51,6 +39,51 @@ const accounts = {
         deleteCookie()
         window.location.href = "/"
     }
+}
+
+function getAccountsTemplate() {
+    return '{{#accounts}}\n' +
+        '    <tr>\n' +
+        '        <td>{{id}}</td>\n' +
+        '        <td>{{accountId}}</td>\n' +
+        '        <td>{{nickName}}</td>\n' +
+        '        <td>{{isAdmin}}</td>\n' +
+        '        <td>{{joinDate}}</td>\n' +
+        '        <td>\n' +
+        '            <button type="button" class="btn btn-primary" id="button-admin" data-id="{{id}}"\n' +
+        '                    data-isadmin="{{isAdmin}}">{{#isAdmin}}권한 삭제{{/isAdmin}}{{^isAdmin}}권한 부여{{/isAdmin}}</button>\n' +
+        '        </td>\n' +
+        '    </tr>\n' +
+        '{{/accounts}}'
+}
+
+function initPermissionButtons() {
+    $(document).ready(() => {
+        $('button[id=button-admin]').on('click', (event) => {
+            const id = $(event.currentTarget).attr('data-id')
+            const isAdmin = $(event.currentTarget).attr('data-isadmin')
+            changePermission(id, isAdmin)
+        })
+    })
+}
+
+function changePermission(id, isAdmin) {
+    const changedPermission = !(isAdmin === 'true')
+    $.ajax({
+        type: 'PATCH',
+        url: `api/account/permission/${id}?isAdmin=${changedPermission}`,
+        contentType: 'application/json; charset=utf-8',
+        headers: {
+            'Authorization': getCookie('accessToken')
+        },
+    }).done(function () {
+        alert('권한 변경을 완료했습니다!')
+        location.reload();
+    }).fail(function (error) {
+        const response = JSON.parse(error.responseText)
+        alert(response.message);
+        window.location.href = "/"
+    });
 }
 
 accounts.init()
